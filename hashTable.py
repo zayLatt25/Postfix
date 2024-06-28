8204de8a670dfe1f929d56c8b36a018b8b925041
M-A-C255 <zaypainglatt04@gmail.com>
2024-06-28 12:58:18 +0800

    implemented hashtable.py class

---
+++ hashTable.py
@@ -0,0 +1,19 @@
+class HashTable:
+    # initialize the hash table with a size of 26 for the capital letters
+    def __init__(self, size):
+        self.size = size
+        self.table = [-1] * size
+    
+    # hash function to get the index of the key
+    def hash_function(self, key):
+        return ord(key) - ord('A')
+    
+    # insert a key-value pair into the hash table
+    def insert(self, key, value):
+        index = self.hash_function(key)
+        self.table[index] = value
+    
+    # search for a key in the hash table
+    def search(self, key):
+        index = self.hash_function(key)
+        return self.table[index]
