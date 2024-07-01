043c70fc7081df19ca35915a8d86facbfd1aa37a
M-A-C255 <zaypainglatt04@gmail.com>
2024-07-02 00:07:01 +0800

    changed function name

--- hashTable.py
+++ hashTable.py
@@ -5,19 +5,19 @@ class HashTable:
         self.table = [None] * size
 
     # hash function to get the index of the key
-    def hash_function(self, key):
+    def hashFunction(self, key):
         return ord(key) - ord("A")
 
     # insert a key-value pair into the hash table
     def insert(self, key, value):
-        index = self.hash_function(key)
+        index = self.hashFunction(key)
         self.table[index] = value
 
     # search for a key in the hash table
     def search(self, key):
-        index = self.hash_function(key)       
+        index = self.hashFunction(key)       
         return self.table[index]
 
     def delete(self, key):
-        index = self.hash_function(key)
+        index = self.hashFunction(key)
         self.table[index] = None
